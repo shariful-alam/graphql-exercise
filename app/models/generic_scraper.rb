7bb0ca01452c5d0ee9ee58c2216104ae329d79be
Shariful Alam <shariful.alam85@gmail.com>
2021-05-19 00:50:13 +0600

    - Updated mutation

--- app/models/generic_scraper.rb
+++ app/models/generic_scraper.rb
@@ -17,31 +17,39 @@ class GenericScraper
     website_url = document.xpath("//div[@class='kCrYT']//a[2]").first['href']
     website_url = (website_url.split('.com').first + '.com').split('=').last
     hours = document.xpath("//div[2]//div[1]//span[2]//span[1]").text
+    website_data =
+      if website_url == 'https://www.livecarraway.com'
+        CarrawayScraper.new(website_url).scrape_data
+      elsif website_url == 'https://exoreston.com'
+        ExonRestonScraper.new(website_url).scrape_data
+      else
+        {}
+      end
     property = Property.where('name ilike :search', { search: property_name }).first
     if property.nil?
-      website_data =
-        if website_url == 'https://www.livecarraway.com'
-          CarrawayScraper.new(website_url).scrape_data
-        elsif website_url == 'https://exoreston.com'
-          ExonRestonScraper.new(website_url).scrape_data
-        else
-          {}
-        end
       Property.new(
-          name: property_name,
-          website_url: website_url,
-          hours: { hours: hours },
-          address: property_address,
-          amenities_url: website_data[:amenities_url],
-          floor_plan_url: website_data[:floor_plan_url],
-          gallery_url: website_data[:gallery_url],
-          contact_us_url: website_data[:contact_us_url],
-          neighborhood_url: website_data[:neighborhood_url],
-          features_url: website_data[:features_url],
-          facebook_url: website_data[:facebook_url],
-          instagram_url: website_data[:instagram_url]
+        name: property_name,
+        website_url: website_url,
+        hours: { hours: hours },
+        address: property_address,
+        amenities_url: website_data[:amenities_url],
+        floor_plan_url: website_data[:floor_plan_url],
+        gallery_url: website_data[:gallery_url],
+        contact_us_url: website_data[:contact_us_url],
+        neighborhood_url: website_data[:neighborhood_url],
+        features_url: website_data[:features_url],
+        facebook_url: website_data[:facebook_url],
+        instagram_url: website_data[:instagram_url]
       )
     else
+      property.amenities_url = website_data[:amenities_url]
+      property.floor_plan_url = website_data[:floor_plan_url]
+      property.gallery_url = website_data[:gallery_url]
+      property.contact_us_url = website_data[:contact_us_url]
+      property.neighborhood_url = website_data[:neighborhood_url]
+      property.features_url = website_data[:features_url]
+      property.facebook_url = website_data[:facebook_url]
+      property.instagram_url = website_data[:instagram_url]
       property
     end
   end
