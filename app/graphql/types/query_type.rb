215e39f1c00dd88d8b4078977c90898519c1e362
Shariful Alam <shariful.alam85@gmail.com>
2021-05-21 01:58:01 +0600

    - Added pagination for query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,12 +5,15 @@ module Types
       argument :propertyName, String, required: true
       argument :startDate, String, required: true
       argument :endDate, String, required: true
+      argument :limit, Integer, required: true
+      argument :offset, Integer, required: true
     end
 
-    def properties(propertyName:, startDate:, endDate:)
+    def properties(propertyName:, startDate:, endDate:, limit:, offset:)
       start_date = Date.parse(startDate)
       end_date = Date.parse(endDate)
-      Property.where('name ilike :name AND updated_at >= :start_date AND updated_at <= :end_date', {name: propertyName, start_date: start_date, end_date: end_date})
+      Property.where(name: propertyName,
+                     updated_at: start_date.beginning_of_day..end_date.end_of_day).limit(limit).offset(offset)
     end
   end
 end
